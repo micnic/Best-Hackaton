de49d70442e932d303eaef3aab8998cc6931667b
John Doe <johndoe@example.com>
2014-05-10 14:04:38 +0300

    create move function

--- public/js/init.js
+++ public/js/init.js
@@ -1,107 +1,64 @@
 var bmpAnimation;
+var stage;
+var tmp
 document.addEventListener('DOMContentLoaded', function () {
 //    var canvas = document.getElementById("game");
 
     var canvas;
-    var stage;
+
     var screen_width;
     var screen_height;
-
 
     var imgMonsterARun = new Image();
 
     function init() {
         canvas = document.getElementById("game");
-
-        imgMonsterARun.onload = handleImageLoad;
-        imgMonsterARun.onerror = handleImageError;
-        imgMonsterARun.src = "public/img/move.png";
-    }
-
-    function reset() {
-        stage.removeAllChildren();
-        createjs.Ticker.removeAllListeners();
-        stage.update();
-    }
-
-    function handleImageLoad(e) {
         startGame();
+//        imgMonsterARun.onload = handleImageLoad;
+//        imgMonsterARun.onerror = handleImageError;
+//        imgMonsterARun.src = "public/img/move.png";
     }
 
     function startGame() {
-        // create a new stage and point it at our canvas:
+//        // create a new stage and point it at our canvas:
         stage = new createjs.Stage(canvas);
 
-        // grab canvas width and height for later calculations:
-        screen_width = canvas.width;
-        screen_height = canvas.height;
-
-        // create spritesheet and assign the associated data.
-        var spriteSheet = new createjs.SpriteSheet({
-//            framerate: 5,
-            // image to use
-            images: ['public/img/move.png', 'public/img/idle.png'],
-            // width, height & registration point of each sprite
-//            frames: {width: 35, height: 35},
-            frames: [
-                // x, y, width, height, imageIndex, regX, regY
-                [0, 0, 35, 35, 0], // 10 images
-//                [0, 0, 25, 35, 1] // 3 images
-            ],
-            animations: {
-                idle: {
-//                    frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-                    frames: [0, 1]
+        var element1 = {
+                images: ['public/img/move.png'],
+                // width, height & registration point of each sprite
+                frames: {width: 35, height: 35},
+                animations: {
+                    // start, end, next, speed
+                    move: [0, 9]
                 },
-                move: [4, 9, 'move', 20]
+                animKey: 'move'
+            },
+            element2 = {
+                images: ['public/img/idle.png'],
+                frames: {width: 25, height: 35},
+                animations: {
+                    // start, end, next, speed
+                    move: [0, 2]
+                },
+                animKey: 'idle'
             }
-        });
 
-        // create a BitmapAnimation instance to display and play back the sprite sheet:
-        bmpAnimation = new createjs.Sprite(spriteSheet);
+        tmp = new BaseModel({arrSprites: [element1, element2]});
 
-        // start playing the first sequence:
-        bmpAnimation.gotoAndPlay("move"); 	//animate
-//        bmpAnimation.gotoAndStop("move"); 	//animate
+        tmp.setAction({action: 'move'});
 
-        bmpAnimation.currentFrame = 0;
-        stage.addChild(bmpAnimation);
-
+        stage.addChild(tmp);
+//
         createjs.Ticker.addEventListener("tick", tick);
         createjs.Ticker.useRAF = true;
-        createjs.Ticker.setFPS(2);
+        createjs.Ticker.setFPS(10);
     }
 
-//called if there is an error loading the image (usually due to a 404)
-    function handleImageError(e) {
-        console.log("Error Loading Image : " + e.target.src);
-    }
-
+//
     function tick() {
-//        // Hit testing the screen width, otherwise our sprite would disappear
-//        if (bmpAnimation.x >= screen_width - 16) {
-//            // We've reached the right side of our screen
-//            // We need to walk left now to go back to our initial position
-//            bmpAnimation.direction = -90;
-//        }
-//
-//        if (bmpAnimation.x < 16) {
-//            // We've reached the left side of our screen
-//            // We need to walk right now
-//            bmpAnimation.direction = 90;
-//        }
-//
-//        // Moving the sprite based on the direction & the speed
-//        if (bmpAnimation.direction == 90) {
-//            bmpAnimation.x += bmpAnimation.vX;
-//        }
-//        else {
-//            bmpAnimation.x -= bmpAnimation.vX;
-//        }
-
-        // update the stage:
         stage.update();
     }
 
+//
     init();
 });
--- public/js/models/BaseModel.js
+++ public/js/models/BaseModel.js
@@ -1,2 +1,77 @@
-window.Button = Button;
+(function () {
+
+    var BaseModel = function (options) {
+        this.initialize(options);
+    }
+
+    var bm = BaseModel.prototype = new createjs.Container(); // inherit from Container
+
+    bm.bnpAnimationObjects = {};
+
+    bm.currentAnimmation = null;
+
+    bm.Container_initialize = bm.initialize;
+
+    bm.initialize = function (options) {
+
+        this._initSprites(options.arrSprites);
+
+//        me.bnpAnimationArr();
+//        this.addChild(this.background, );
+//        this.on("click", this.handleClick);
+        this.on("tick", this.handleTick);
+    }
+
+    bm.handleTick = function() {
+//        console.log('aaaa')
+    }
+
+    bm._initSprites = function (arrSprites) {
+        var me = this;
+        arrSprites.forEach(function (element, index) {
+
+            var spriteSheet = new createjs.SpriteSheet(element);
+
+            var animation = new createjs.Sprite(spriteSheet);
+
+            me.bnpAnimationObjects[element.animKey] = animation;
+
+        });
+
+    }
+
+    bm.setAction = function(direction) {
+
+        if(this.currentAnimmation) {
+            stage.removeChild(this.currentAnimmation);
+        }
+
+        if(direction.transform) {
+            //TODO transform
+        } else {
+            this.currentAnimmation =  this.bnpAnimationObjects[direction.action];
+
+            this.currentAnimmation.gotoAndPlay(direction.action);
+        }
+
+        stage.addChild(this.currentAnimmation);
+    }
+
+
+//
+    bm.handleClick = function (event) {
+        console.log('click');
+    }
+//
+//    bm.handleTick = function (event) {
+//        console.log('handle click')
+//    }
+//
+//    bm.move = function (direction) {
+//        this.x = this.x + 1;
+//        this.y = this.y + 1;
+//        console.log(direction);
+//    }
+
+    window.BaseModel = BaseModel;
 }());
